2dacca1c69072c2e230b0995f8380ca02b400312
David Stanley <davidmstanleyjr@gmail.com>
2020-05-28 18:43:33 -0400

    even more variables.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //These are my DOM objects
 const pokeName = document.querySelector('.poke-name');
+const mainScreen = document.querySelector('.main-screen');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
 const pokeBackImage = document.querySelector('.poke-back-image');
@@ -7,3 +8,6 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
+const pokeListItems = document.querySelectorAll('.list-item');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
