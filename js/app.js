2e2a299f7c4d2cc55308913b6eed5c841e4f55d9
David Stanley <davidmstanleyjr@gmail.com>
2020-05-29 12:59:04 -0400

    added logic for pokemon attritbutes and fetching data from the api.

--- js/app.js
+++ js/app.js
@@ -69,3 +69,30 @@ const fetchPokeList = (url) => {
 		}
 	});
 };
+
+//This fetches the pokeData
+const fetchPokeData = (id) => {
+	fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()).then((data) => {
+		resetScreen();
+
+		const dataTypes = data['types'];
+		const dataFirstType = dataTypes[0];
+		const dataSecondType = dataTypes[1];
+		pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
+		if (dataSecondType) {
+			pokeTypeTwo.classList.remove('hide');
+			pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
+		} else {
+			pokeTypeTwo.classList.add('hide');
+			pokeTypeTwo.textContent = '';
+		}
+		mainScreen.classList.add(dataFirstType['type']['name']);
+		//Pokemon attributes
+		pokeName.textContent = capitalize(data['name']);
+		pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+		pokeWeight.textContent = data['weight'];
+		pokeHeight.textContent = data['height'];
+		pokeFrontImage.src = data['sprites']['front_default'] || '';
+		pokeBackImage.src = data['sprites']['back_default'] || '';
+	});
+};
