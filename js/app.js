698c43b1f367028412ef4cd2178a1b1ac27c88f2
David Stanley <davidmstanleyjr@gmail.com>
2020-05-28 23:46:45 -0400

    wrote fetchPokeList logic.

--- js/app.js
+++ js/app.js
@@ -46,3 +46,26 @@ const resetScreen = () => {
 		mainScreen.classList.remove(type);
 	}
 };
+
+//This fetches the list of Pokemon
+const fetchPokeList = (url) => {
+	fetch(url).then((res) => res.json()).then((data) => {
+		const { results, previous, next } = data;
+		prevUrl = previous;
+		nextUrl = next;
+
+		for (let i = 0; i < pokeListItems.length; i++) {
+			const pokeListItem = pokeListItems[i];
+			const resultData = results[i];
+
+			if (resultData) {
+				const { name, url } = resultData;
+				const urlArray = url.split('/');
+				const id = urlArray[urlArray.length - 2];
+				pokeListItem.textContent = id + ' . ' + capitalize(name);
+			} else {
+				pokeListItem.textContent = '';
+			}
+		}
+	});
+};
