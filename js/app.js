e43bbd99760bf35070e5177491abab96f2a3c4a5
David Stanley <davidmstanleyjr@gmail.com>
2020-05-28 22:34:58 -0400

    did a capitalize function and a resetScreen function.

--- js/app.js
+++ js/app.js
@@ -35,3 +35,14 @@ const TYPES = [
 ];
 let prevUrl = null;
 let nextUrl = null;
+
+//Functions
+
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+const resetScreen = () => {
+	mainScreen.classList.remove('hide');
+	for (const type of TYPES) {
+		mainScreen.classList.remove(type);
+	}
+};
